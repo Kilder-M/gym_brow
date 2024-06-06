15f1176cdb832d77428f5434a8b2eac94bf269fc
Kilder <kilder.vsp@hotmail.com>
2024-06-05 22:46:46 -0300

    excluir está funcionando

--- lib/app/presentation/workout/controllers/workout_view_controller.dart
+++ lib/app/presentation/workout/controllers/workout_view_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:gymbrow/app/core/utils/models/weekday_dart';
 import 'package:gymbrow/app/domain/entities/workout_entity.dart';
+import 'package:gymbrow/app/domain/usecases/deletes/delete_workout_usecase.dart';
 import 'package:gymbrow/app/domain/usecases/gets/get_workout_list_by_weekday_usecase.dart';
 
 class WorkoutViewController extends GetxController {
@@ -11,10 +12,16 @@ class WorkoutViewController extends GetxController {
   final GetWorkoutListByWeekdayUseCase _getWorkoutListByweekday =
       Get.find<GetWorkoutListByWeekdayUseCase>();
 
-  Future<List<WorkoutEntity>> listar() async {
-    var test = await _getWorkoutListByweekday(weekday.id);
-    print(test);
-    workoutList.value = test;
-    return test;
+  final DeleteWorkoutUsecase _deleteWorkoutUsecase =
+      Get.find<DeleteWorkoutUsecase>();
+
+  Future<List<WorkoutEntity>> listWorkouts() async {
+    return workoutList.value = await _getWorkoutListByweekday(weekday.id);
+  }
+
+  Future<bool> deleteWorkout(WorkoutEntity workoutEntity) async {
+    bool result = await _deleteWorkoutUsecase(workoutEntity);
+    workoutList.remove(workoutEntity);
+    return result;
   }
 }
--- lib/app/presentation/workout/views/workout_view.dart
+++ lib/app/presentation/workout/views/workout_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gymbrow/app/domain/entities/workout_entity.dart';
 import 'package:gymbrow/app/presentation/bases/gb_base_view.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:gymbrow/app/presentation/workout/controllers/workout_view_controller.dart';
 
 class WorkoutView extends GBBaseView<WorkoutViewController> {
@@ -12,7 +13,7 @@ class WorkoutView extends GBBaseView<WorkoutViewController> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Get.toNamed('/workout_form',arguments: controller.weekday);
+          Get.toNamed('/workout_form', arguments: controller.weekday);
         },
         child: const Icon(Icons.add),
       ),
@@ -22,91 +23,126 @@ class WorkoutView extends GBBaseView<WorkoutViewController> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4),
         child: FutureBuilder(
-          future: controller.listar(),
+          future: controller.listWorkouts(),
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
               return const Center(
                 child: CircularProgressIndicator(),
               );
             } else {
-              return ListView.builder(
-                itemCount: controller.workoutList.length,
-                itemBuilder: (context, index) {
-                  WorkoutEntity workoutEntity = controller.workoutList[index];
-                  RxBool isDone = workoutEntity.isDone.obs;
-                  return Card(
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-                      title: Row(
-                        children: [
-                          Expanded(
-                            child: Tooltip(
-                              message: workoutEntity.name,
-                              child: Text(
-                                workoutEntity.name,
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
+              return Obx(
+                () => ListView.builder(
+                  itemCount: controller.workoutList.length,
+                  itemBuilder: (context, index) {
+                    WorkoutEntity workoutEntity = controller.workoutList[index];
+                    RxBool isDone = workoutEntity.isDone.obs;
+                    return Card(
+                      child: ListTile(
+                        onLongPress: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text(workoutEntity.name),
+                                content: Text(
+                                  AppLocalizations.of(context)!
+                                      .what_would_you_like_to_do,
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      controller.deleteWorkout(workoutEntity);
+                                      Get.back();
+                                    },
+                                    child: Text(
+                                      AppLocalizations.of(context)!.delete,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      AppLocalizations.of(context)!.edit,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 8),
+                        title: Row(
+                          children: [
+                            Expanded(
+                              child: Tooltip(
+                                message: workoutEntity.name,
+                                child: Text(
+                                  workoutEntity.name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
                               ),
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Obx(
-                                () => SizedBox(
-                                  width: 50,
-                                  height: 30,
-                                  child: FittedBox(
-                                    fit: BoxFit.fill,
-                                    child: Switch(
-                                      value: isDone.value,
-                                      onChanged: (bool value) {
-                                        isDone.value = value;
-                                      },
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Obx(
+                                  () => SizedBox(
+                                    width: 50,
+                                    height: 30,
+                                    child: FittedBox(
+                                      fit: BoxFit.fill,
+                                      child: Switch(
+                                        value: isDone.value,
+                                        onChanged: (bool value) {
+                                          isDone.value = value;
+                                        },
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
+                          ],
+                        ),
+                        leading: const CircleAvatar(
+                          child: Icon(
+                            Icons.fitness_center_outlined,
+                            size: 12,
                           ),
-                        ],
-                      ),
-                      leading: const CircleAvatar(
-                        child: Icon(
-                          Icons.fitness_center_outlined,
-                          size: 12,
+                        ),
+                        subtitle: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: IconLabelValue(
+                                tooltipMessage: '',
+                                icon: Icons.repeat,
+                                value:
+                                    '${workoutEntity.series} x ${workoutEntity.repetitions}',
+                              ),
+                            ),
+                            Expanded(
+                              child: IconLabelValue(
+                                tooltipMessage: '',
+                                icon: Icons.timer_outlined,
+                                value:
+                                    '${workoutEntity.restTime.toString()}min',
+                              ),
+                            ),
+                            Expanded(
+                              child: IconLabelValue(
+                                tooltipMessage: '',
+                                icon: Icons.fitness_center,
+                                value: '${workoutEntity.weight}kg',
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: IconLabelValue(
-                              tooltipMessage: '',
-                              icon: Icons.repeat,
-                              value:
-                                  '${workoutEntity.series} x ${workoutEntity.repetitions}',
-                            ),
-                          ),
-                          Expanded(
-                            child: IconLabelValue(
-                              tooltipMessage: '',
-                              icon: Icons.timer_outlined,
-                              value: '${workoutEntity.restTime.toString()}min',
-                            ),
-                          ),
-                          Expanded(
-                            child: IconLabelValue(
-                              tooltipMessage: '',
-                              icon: Icons.fitness_center,
-                              value: '${workoutEntity.weight}kg',
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               );
             }
           },
