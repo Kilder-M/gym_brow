79447771197450b258d8988547501096265bb7ad
Kilder <kilder.vsp@hotmail.com>
2024-06-28 00:38:19 -0300

    DarkMode

--- lib/app/presentation/home/controllers/home_controller.dart
+++ lib/app/presentation/home/controllers/home_controller.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gymbrow/app/presentation/home/views/settings_tab.dart';
 import 'package:gymbrow/app/presentation/home/views/workout_days_tab.dart';
 
 class HomeController extends GetxController {
-
   var selectedItem = 0.obs;
+
+  RxBool isDarkTheme = false.obs ;
 
   List<Widget> tabs = [
     const WorkoutDaysTab(),
     Container(
       child: Center(child: Text('2')),
     ),
-    Container(
-      child: Center(child: Text('3')),
-    ),
+    const SettingsTab(),
   ];
-
-
 }
---
+++ lib/app/presentation/home/views/settings_tab.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gymbrow/app/presentation/bases/gb_base_view.dart';
+import 'package:gymbrow/app/presentation/home/controllers/home_controller.dart';
+
+class SettingsTab extends GBBaseView<HomeController> {
+  const SettingsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.settings),
+      ),
+      body: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+        trailing: SizedBox(
+          width: 50,
+          height: 30,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Obx(
+              () => Switch(
+                value: controller.isDarkTheme.value,
+                onChanged: (bool value) {
+                  controller.isDarkTheme.value = value;
+                  Get.changeTheme(controller.isDarkTheme.value
+                      ? ThemeData.dark()
+                      : ThemeData.light());
+                },
+              ),
+            ),
+          ),
+        ),
+        title: const Row(
+          children: [
+            Expanded(
+              child: Text(
+                //Internacionalizar
+                'Dark mode',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
+        leading: const CircleAvatar(
+          child: Icon(
+            Icons.dark_mode,
+            size: 12,
+          ),
+        ),
+      ),
+    );
+  }
+}
